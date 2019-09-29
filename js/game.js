c0726ceb00b45a15ff5e4ffa9d7f8a948ed47e20
federicomichela <federico.michela@gmail.com>
2019-09-29 18:04:34 +0100

    fixed bug + added animations + bet updates

--- js/game.js
+++ js/game.js
@@ -3,13 +3,27 @@ class Roulette {
         this._map = map;
         this._wheel = wheel;
         this._balance = BALANCE;
+        this._bets = {};
+        this._betsTotal = 0;
     }
 
     initialiseGame() {
         this._renderWheel();
         this._updateGameInfos();
 
+        document.addEventListener("click", this._dispatchEvent.bind(this));
         document.querySelector("#placeBetBtn").addEventListener("click", this._placeBet.bind(this));
+    }
+
+    _dispatchEvent(event) {
+        if (event.target.classList.contains("chip")) {
+            this._selectChip(event)
+        }
+        if (["td", "th"].indexOf(event.target.tagName.toLowerCase()) > -1) {
+            if (event.target.querySelector(".bet")) {
+                this._addBet(event);
+            }
+        }
     }
 
     _renderWheel() {
@@ -30,15 +44,54 @@ class Roulette {
         }
     }
 
-    _updateGameInfos(newBalance, newPayout) {
-        if (newBalance) {
-            this._balance = newBalance;
+    _updateGameInfos(config) {
+        config = config || {};
+
+        let balance = config.balance || this._balance;
+        let bet = config.bet || this._betsTotal;
+        let payout = config.payout || 0;
+
+        if (this._balance != balance) {
+            this._balance = config.newBalance;
         }
 
-        newPayout = newPayout || 0;
-
         document.querySelector("#balance span").innerText = this._balance.formatCurrency();
-        document.querySelector("#payout span").innerText = newPayout.formatCurrency();
+        document.querySelector("#betTotal span").innerText = bet.formatCurrency();
+        document.querySelector("#payout span").innerText = payout.formatCurrency();
+    }
+
+    _selectChip(event) {
+        let chip = event.target;
+        let allChips = [...document.getElementsByClassName("chip")];
+        let table = document.querySelector("#carpet table");
+
+        allChips.forEach(currentChip => currentChip.classList.remove("selected"));
+
+        chip.classList.add("selected");
+
+        if (!table.classList.contains("active")) {
+            table.classList.add("active");
+        }
+    }
+
+    _addBet(event) {
+        let target = event.target;
+        let chipSelected = document.querySelector(".chip.selected");
+        let chipSelectedValue = parseFloat(chipSelected.getAttribute("value"));
+        let betOnTarget = target.querySelector(".bet");
+        let betOnTargetValue = betOnTarget.innerText;
+
+        if (betOnTargetValue.length) {
+            betOnTargetValue = parseFloat(betOnTarget.innerText.split("+")[1]);
+        } else {
+            betOnTargetValue = 0;
+        }
+
+        betOnTargetValue += chipSelectedValue;
+        betOnTarget.innerText = `+${betOnTargetValue}`;
+
+        this._betsTotal += betOnTargetValue;
+        this._updateGameInfos();
     }
 
     _placeBet() {
@@ -56,14 +109,27 @@ class Roulette {
             duration: duration
         };
 
-        setTimeout(this._updatePosition.bind(this, rotation), duration);
+        // spin wheel
+        setTimeout(this._onWheelSpinCompleted.bind(this, rotation), duration);
         this._wheel.animate(keyFrames, options);
 
+        // update balance
+        this._balance -= this._betsTotal;
+        this._updateGameInfos();
+
+        // DEBUGGING PURPOSES
         console.info(randomNumber);
         console.info(this._map[randomNumber]);
     }
 
-    _updatePosition(rotation) {
+    _onWheelSpinCompleted(rotation) {
+        // update wheel position
         this._wheel.style.transform = `rotateZ(${rotation}deg)`;
+
+        this._computeWin();
+    }
+
+    _computeWin() {
+
     }
 }
